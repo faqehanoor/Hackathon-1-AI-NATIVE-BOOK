8821c80dbe66b8f7ffb3e1ba2690237ca04d5ac5
Faqeha Noor <162030362+faqehanoor@users.noreply.github.com>
2025-12-25 15:32:30 +0500

    Update docusaurus.config.ts

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://faqehanoor.github.io',
+  url: 'https://hackathon-1-ai-native-book.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
